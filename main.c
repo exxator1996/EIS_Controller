d401232471c4b934043e0a8aacbcad6e32e1a561
Niklas Guntelmann <niklasguntelmann@live.de>
2024-03-26 10:07:36 +0100

    Completed Output Mode check

--- main.c
+++ main.c
@@ -45,10 +45,22 @@
 #include <stdint.h>
 
 // Betriebsmodi
-#define MODE_IDLE 0x0
-#define MODE_RL   0x1
-#define MODE_LR   0x2
-#define MODE_BP   0x3
+#define MODE_RL 0x0
+#define MODE_LR 0x1
+#define MODE_BP 0x3
+
+// State Idle
+#define MODE_IDLE     0x4
+// States MODE RL
+#define MODE_RL_OFF   0x5
+#define MODE_RL_ON    0x6
+// States MODE LR
+#define MODE_LR_OFF   0x7
+#define MODE_LR_ON    0x8
+// States MODE BP
+#define MODE_BP_OFF   0x9
+#define MODE_BP_RL_ON 0x10
+#define MODE_BP_LR_ON 0x11
 
 // Output pin setter Traco 1
 #define PIN_PRI_1_PS  PORT0_OMR_PS0_Msk
@@ -57,7 +69,7 @@
 // Output pin setter Traco 2
 #define PIN_PRI_2_PS  PORT0_OMR_PS7_Msk
 #define PIN_SEK_2_PS  PORT0_OMR_PS8_Msk
-#define PIN_CRTL_3_PS PORT0_OMR_PS9_Msk
+#define PIN_CRTL_2_PS PORT0_OMR_PS9_Msk
 // Output pin resetter Traco 1
 #define PIN_PRI_1_PR  PORT0_OMR_PR0_Msk
 #define PIN_SEK_1_PR  PORT0_OMR_PR5_Msk
@@ -65,22 +77,24 @@
 // Output pin resetter Traco 2
 #define PIN_PRI_2_PR  PORT0_OMR_PR7_Msk
 #define PIN_SEK_2_PR  PORT0_OMR_PR8_Msk
-#define PIN_CRTL_3_PR PORT0_OMR_PR9_Msk
+#define PIN_CRTL_2_PR PORT0_OMR_PR9_Msk
 
-// Output states MODE_RL
-#define MODE_RL_OFF   (PIN_PRI_1_PS | PIN_SEK_1_PR | PIN_CRTL_1_PS | PIN_PRI_2_PR | PIN_SEK_2_PR | PIN_CRLT_2_PR)
-#define MODE_RL_ON    (PIN_PRI_1_PS | PIN_SEK_1_PS | PIN_CRTL_1_PS | PIN_PRI_2_PR | PIN_SEK_2_PR | PIN_CRLT_2_PR)
-// Output state MODE_LR
-#define MODE_LR_OFF   (PIN_PRI_1_PR | PIN_SEK_1_PR | PIN_CRTL_1_PR | PIN_PRI_2_PS | PIN_SEK_2_PR | PIN_CRLT_2_PS)
-#define MODE_LR_ON    (PIN_PRI_1_PR | PIN_SEK_1_PR | PIN_CRTL_1_PR | PIN_PRI_2_PS | PIN_SEK_2_PS | PIN_CRLT_2_PS)
-// Output state MODE_BP
-#define MODE_BP_OFF   (PIN_PRI_1_PS | PIN_SEK_1_PR | PIN_CRTL_1_PS | PIN_PRI_2_PS | PIN_SEK_2_PR | PIN_CRLT_2_PS)
-#define MODE_BP_RL_ON (PIN_PRI_1_PS | PIN_SEK_1_PS | PIN_CRTL_1_PS | PIN_PRI_2_PS | PIN_SEK_2_PR | PIN_CRLT_2_PS)
-#define MODE_BP_LR_ON (PIN_PRI_1_PS | PIN_SEK_1_PR | PIN_CRTL_1_PS | PIN_PRI_2_PS | PIN_SEK_2_PS | PIN_CRLT_2_PS)
+// Output states MODE_IDLE (all off)
+#define MODE_IDLE_OUT     (PIN_PRI_1_PR | PIN_SEK_1_PR | PIN_CRTL_1_PR | PIN_PRI_2_PR | PIN_SEK_2_PR | PIN_CRTL_2_PR)
+// Output states MODE_RL _PS = HIGH _PR = LOW
+#define MODE_RL_OFF_OUT   (PIN_PRI_1_PS | PIN_SEK_1_PR | PIN_CRTL_1_PS | PIN_PRI_2_PR | PIN_SEK_2_PR | PIN_CRTL_2_PR)
+#define MODE_RL_ON_OUT    (PIN_PRI_1_PS | PIN_SEK_1_PS | PIN_CRTL_1_PS | PIN_PRI_2_PR | PIN_SEK_2_PR | PIN_CRTL_2_PR)
+// Output state MODE_LR _PS = HIGH _PR = LOW
+#define MODE_LR_OFF_OUT   (PIN_PRI_1_PR | PIN_SEK_1_PR | PIN_CRTL_1_PR | PIN_PRI_2_PS | PIN_SEK_2_PR | PIN_CRTL_2_PS)
+#define MODE_LR_ON_OUT    (PIN_PRI_1_PR | PIN_SEK_1_PR | PIN_CRTL_1_PR | PIN_PRI_2_PS | PIN_SEK_2_PS | PIN_CRTL_2_PS)
+// Output state MODE_BP _PS = HIGH _PR = LOW
+#define MODE_BP_OFF_OUT   (PIN_PRI_1_PS | PIN_SEK_1_PR | PIN_CRTL_1_PS | PIN_PRI_2_PS | PIN_SEK_2_PR | PIN_CRTL_2_PS)
+#define MODE_BP_RL_ON_OUT (PIN_PRI_1_PS | PIN_SEK_1_PS | PIN_CRTL_1_PS | PIN_PRI_2_PS | PIN_SEK_2_PR | PIN_CRTL_2_PS)
+#define MODE_BP_LR_ON_OUT (PIN_PRI_1_PS | PIN_SEK_1_PR | PIN_CRTL_1_PS | PIN_PRI_2_PS | PIN_SEK_2_PS | PIN_CRTL_2_PS)
 
 void ccu4_0_SR0_INTERRUPT_HANDLER() {
   XMC_CCU4_SLICE_ClearEvent(timerMSB_HW, XMC_CCU4_SLICE_IRQ_ID_PERIOD_MATCH);
-  PORT0->OMR |= (PORT0_OMR_PS0_Msk | PORT0_OMR_PR0_Msk);
+  // PORT0->OMR |= (PORT0_OMR_PS0_Msk | PORT0_OMR_PR0_Msk);
 }
 
 int main(void) {
@@ -101,6 +115,10 @@ int main(void) {
   XMC_CCU4_SLICE_SetTimerPeriodMatch(timerLSB_HW, lsbValue);
   XMC_CCU4_SLICE_SetTimerPeriodMatch(timerMSB_HW, msbValue);
   XMC_CCU4_EnableShadowTransfer(ccu4_0_HW, (XMC_CCU4_SHADOW_TRANSFER_SLICE_0 | XMC_CCU4_SHADOW_TRANSFER_SLICE_1));
+
+  // Test outputs
+  PORT0->OMR = MODE_BP_LR_ON_OUT;
+
   for (;;) {
   }
 }
