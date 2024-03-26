8aa5bf46aea6d258a9e2d08d7f05134f88a093ac
Niklas Guntelmann <niklasguntelmann@live.de>
2024-03-26 11:42:28 +0100

    Alle modi funktionieren korrekt

--- main.c
+++ main.c
@@ -44,23 +44,24 @@
 #include "xmc_ccu4.h"
 #include <stdint.h>
 
-// Betriebsmodi
-#define MODE_RL 0x0
-#define MODE_LR 0x1
-#define MODE_BP 0x3
-
+// Operating modes
+#define MODE_RL        0x0
+#define MODE_LR        0x1
+#define MODE_BP        0x3
+#define MODE_IDLE      0x4
 // State Idle
-#define MODE_IDLE     0x4
+#define MODE_IDLE_OFF  0x5
 // States MODE RL
-#define MODE_RL_OFF   0x5
-#define MODE_RL_ON    0x6
+#define MODE_RL_OFF    0x6
+#define MODE_RL_ON     0x7
 // States MODE LR
-#define MODE_LR_OFF   0x7
-#define MODE_LR_ON    0x8
+#define MODE_LR_OFF    0x8
+#define MODE_LR_ON     0x9
 // States MODE BP
-#define MODE_BP_OFF   0x9
-#define MODE_BP_RL_ON 0x10
-#define MODE_BP_LR_ON 0x11
+#define MODE_BP_RL_OFF 0x10
+#define MODE_BP_RL_ON  0x11
+#define MODE_BP_LR_OFF 0x12
+#define MODE_BP_LR_ON  0x13
 
 // Output pin setter Traco 1
 #define PIN_PRI_1_PS  PORT0_OMR_PS0_Msk
@@ -80,7 +81,7 @@
 #define PIN_CRTL_2_PR PORT0_OMR_PR9_Msk
 
 // Output states MODE_IDLE (all off)
-#define MODE_IDLE_OUT     (PIN_PRI_1_PR | PIN_SEK_1_PR | PIN_CRTL_1_PR | PIN_PRI_2_PR | PIN_SEK_2_PR | PIN_CRTL_2_PR)
+#define MODE_IDLE_OFF_OUT (PIN_PRI_1_PR | PIN_SEK_1_PR | PIN_CRTL_1_PR | PIN_PRI_2_PR | PIN_SEK_2_PR | PIN_CRTL_2_PR)
 // Output states MODE_RL _PS = HIGH _PR = LOW
 #define MODE_RL_OFF_OUT   (PIN_PRI_1_PS | PIN_SEK_1_PR | PIN_CRTL_1_PS | PIN_PRI_2_PR | PIN_SEK_2_PR | PIN_CRTL_2_PR)
 #define MODE_RL_ON_OUT    (PIN_PRI_1_PS | PIN_SEK_1_PS | PIN_CRTL_1_PS | PIN_PRI_2_PR | PIN_SEK_2_PR | PIN_CRTL_2_PR)
@@ -92,9 +93,94 @@
 #define MODE_BP_RL_ON_OUT (PIN_PRI_1_PS | PIN_SEK_1_PS | PIN_CRTL_1_PS | PIN_PRI_2_PS | PIN_SEK_2_PR | PIN_CRTL_2_PS)
 #define MODE_BP_LR_ON_OUT (PIN_PRI_1_PS | PIN_SEK_1_PR | PIN_CRTL_1_PS | PIN_PRI_2_PS | PIN_SEK_2_PS | PIN_CRTL_2_PS)
 
+// Operating mode variable
+uint16_t mode = MODE_IDLE;
+// State variable
+uint16_t state = MODE_IDLE_OFF;
+
 void ccu4_0_SR0_INTERRUPT_HANDLER() {
   XMC_CCU4_SLICE_ClearEvent(timerMSB_HW, XMC_CCU4_SLICE_IRQ_ID_PERIOD_MATCH);
-  // PORT0->OMR |= (PORT0_OMR_PS0_Msk | PORT0_OMR_PR0_Msk);
+  // Set next state
+  switch (mode) {
+  case MODE_RL:
+    switch (state) {
+    case MODE_RL_OFF:
+      state = MODE_RL_ON;
+      break;
+    case MODE_RL_ON:
+      state = MODE_RL_OFF;
+      break;
+    // When switching from other operatig mode or idle
+    default:
+      state = MODE_RL_OFF;
+    }
+    break;
+  case MODE_LR:
+    switch (state) {
+    case MODE_LR_OFF:
+      state = MODE_LR_ON;
+      break;
+    case MODE_LR_ON:
+      state = MODE_LR_OFF;
+      break;
+    // When switching from other operatig mode or idle
+    default:
+      state = MODE_LR_OFF;
+    }
+    break;
+  case MODE_BP:
+    switch (state) {
+    case MODE_BP_RL_OFF:
+      state = MODE_BP_RL_ON;
+      break;
+    case MODE_BP_RL_ON:
+      state = MODE_BP_LR_OFF;
+      break;
+    case MODE_BP_LR_OFF:
+      state = MODE_BP_LR_ON;
+      break;
+    case MODE_BP_LR_ON:
+      state = MODE_BP_RL_OFF;
+      break;
+    // When switching from other operatig mode or idle
+    default:
+      state = MODE_BP_RL_OFF;
+    }
+    break;
+  case MODE_IDLE:
+    state = MODE_IDLE_OFF;
+    break;
+  }
+  // Set output state
+  switch (state) {
+  case MODE_IDLE_OFF:
+    PORT0->OMR = MODE_IDLE_OFF_OUT;
+    break;
+  case MODE_RL_OFF:
+    PORT0->OMR = MODE_RL_OFF_OUT;
+    break;
+  case MODE_RL_ON:
+    PORT0->OMR = MODE_RL_ON_OUT;
+    break;
+  case MODE_LR_OFF:
+    PORT0->OMR = MODE_LR_OFF_OUT;
+    break;
+  case MODE_LR_ON:
+    PORT0->OMR = MODE_LR_ON_OUT;
+    break;
+  case MODE_BP_RL_OFF:
+    PORT0->OMR = MODE_BP_OFF_OUT;
+    break;
+  case MODE_BP_RL_ON:
+    PORT0->OMR = MODE_BP_RL_ON_OUT;
+    break;
+  case MODE_BP_LR_OFF:
+    PORT0->OMR = MODE_BP_OFF_OUT;
+    break;
+  case MODE_BP_LR_ON:
+    PORT0->OMR = MODE_BP_LR_ON_OUT;
+    break;
+  }
 }
 
 int main(void) {
@@ -105,6 +191,10 @@ int main(void) {
   if (result != CY_RSLT_SUCCESS) {
     CY_ASSERT(0);
   }
+  // set operating mode
+  mode = MODE_BP;
+  // Set default output
+  PORT0->OMR = MODE_IDLE_OFF_OUT;
 
   uint32_t periodValue = 16000U - 1;
   uint16_t msbValue = (uint16_t)(periodValue >> 16);
@@ -115,9 +205,6 @@ int main(void) {
   XMC_CCU4_SLICE_SetTimerPeriodMatch(timerLSB_HW, lsbValue);
   XMC_CCU4_SLICE_SetTimerPeriodMatch(timerMSB_HW, msbValue);
   XMC_CCU4_EnableShadowTransfer(ccu4_0_HW, (XMC_CCU4_SHADOW_TRANSFER_SLICE_0 | XMC_CCU4_SHADOW_TRANSFER_SLICE_1));
-
-  // Test outputs
-  PORT0->OMR = MODE_BP_LR_ON_OUT;
 
   for (;;) {
   }
