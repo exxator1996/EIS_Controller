ad0791143cc70dfa81df8cda267945ec92885a94
Niklas Guntelmann <niklasguntelmann@live.de>
2024-04-05 09:19:27 +0200

    Pins an platinen design angepasst

--- modes_states_pins.h
+++ modes_states_pins.h
@@ -9,20 +9,20 @@
 #define MODE_IDLE 3
 
 // Output pin setter Traco 1
-#define PIN_PRI_1_ON   PORT0_OMR_PS0_Msk
-#define PIN_SEK_1_ON   PORT0_OMR_PS5_Msk
-#define PIN_CRTL_1_ON  PORT0_OMR_PS6_Msk
+#define PIN_PRI_1_ON   PORT0_OMR_PS7_Msk
+#define PIN_SEK_1_ON   PORT0_OMR_PS6_Msk
+#define PIN_CRTL_1_ON  PORT0_OMR_PS8_Msk
 // Output pin setter Traco 2
-#define PIN_PRI_2_ON   PORT0_OMR_PS7_Msk
-#define PIN_SEK_2_ON   PORT0_OMR_PS8_Msk
+#define PIN_PRI_2_ON   PORT0_OMR_PS14_Msk
+#define PIN_SEK_2_ON   PORT0_OMR_PS15_Msk
 #define PIN_CRTL_2_ON  PORT0_OMR_PS9_Msk
 // Output pin resetter Traco 1
-#define PIN_PRI_1_OFF  PORT0_OMR_PR0_Msk
-#define PIN_SEK_1_OFF  PORT0_OMR_PR5_Msk
-#define PIN_CRTL_1_OFF PORT0_OMR_PR6_Msk
+#define PIN_PRI_1_OFF  PORT0_OMR_PR7_Msk
+#define PIN_SEK_1_OFF  PORT0_OMR_PR6_Msk
+#define PIN_CRTL_1_OFF PORT0_OMR_PR8_Msk
 // Output pin resetter Traco 2
-#define PIN_PRI_2_OFF  PORT0_OMR_PR7_Msk
-#define PIN_SEK_2_OFF  PORT0_OMR_PR8_Msk
+#define PIN_PRI_2_OFF  PORT0_OMR_PR14_Msk
+#define PIN_SEK_2_OFF  PORT0_OMR_PR15_Msk
 #define PIN_CRTL_2_OFF PORT0_OMR_PR9_Msk
 
 // Output states MODE_IDLE (all off)
@@ -37,5 +37,6 @@
 #define MODE_BP_OFF_OUT   (PIN_PRI_1_ON | PIN_SEK_1_OFF | PIN_CRTL_1_ON | PIN_PRI_2_ON | PIN_SEK_2_OFF | PIN_CRTL_2_ON)
 #define MODE_BP_RL_ON_OUT (PIN_PRI_1_ON | PIN_SEK_1_ON | PIN_CRTL_1_ON | PIN_PRI_2_ON | PIN_SEK_2_OFF | PIN_CRTL_2_ON)
 #define MODE_BP_LR_ON_OUT (PIN_PRI_1_ON | PIN_SEK_1_OFF | PIN_CRTL_1_ON | PIN_PRI_2_ON | PIN_SEK_2_ON | PIN_CRTL_2_ON)
+
 
 #endif
